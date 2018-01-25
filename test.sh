1bbdfabd1da3c29ede8434e85c8b768b05765949
Anton Medvedev <anton@medv.io>
2018-01-26 00:35:55 +0700

    Fix tests

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-alias fx='node index.js'
-echo '[{"greeting": "hello world"}]' | fx
-echo '{"key": "value"}' | fx "x => x.key"
-echo '[1, 2, 3, 4, 5]' | fx "this.map(x => x * this.length)"
-echo '[1, 2, 3, 4, 5]' | fx "for (let i of this) if (i % 2 == 0) yield i"
-echo '{"items": ["foo", "bar"]}' | fx "this.items" "yield* this; yield 'baz'"
+readonly fx='node index.js'
+
+echo '[{"greeting": "hello world"}]' | $fx
+echo '{"key": "value"}' | $fx "x => x.key"
+echo '[1, 2, 3, 4, 5]' | $fx "this.map(x => x * this.length)"
+echo '[1, 2, 3, 4, 5]' | $fx "for (let i of this) if (i % 2 == 0) yield i"
+echo '{"items": ["foo", "bar"]}' | $fx "this.items" "yield* this; yield 'baz'"
