8ad28dec6c39f45483aa126cd37b4f9484127c55
Ryan Eno <ryan.eno@elastic.co>
2019-11-17 16:01:52 -1000

    Revert "Updated defualt color scheme and indent"
    This reverts commit 74e06a6ef560843b520540f73cbbd93f8795a401.

--- config.js
+++ config.js
@@ -2,26 +2,25 @@
 const chalk = require('chalk')
 const noop = x => x
 const list = {
-  fg: 'cyan',
-  bg: 'blue',
+  fg: 'black',
+  bg: 'cyan',
   selected: {
-    fg: 'brightcyan',
-    bg: 'brightblue'
+    bg: 'magenta'
   }
 }
 
 module.exports = {
-  space:            global.FX_STYLE_SPACE             || 4,
+  space:            global.FX_STYLE_SPACE             || 2,
   null:             global.FX_STYLE_NULL              || chalk.grey.bold,
-  number:           global.FX_STYLE_NUMBER            || chalk.magenta,
-  boolean:          global.FX_STYLE_BOOLEAN           || chalk.blue.bold,
-  string:           global.FX_STYLE_STRING            || chalk.green,
-  key:              global.FX_STYLE_KEY               || chalk.cyan,
-  bracket:          global.FX_STYLE_BRACKET           || chalk.grey,
-  comma:            global.FX_STYLE_COMMA             || chalk.grey,
+  number:           global.FX_STYLE_NUMBER            || chalk.cyan.bold,
+  boolean:          global.FX_STYLE_BOOLEAN           || chalk.yellow.bold,
+  string:           global.FX_STYLE_STRING            || chalk.green.bold,
+  key:              global.FX_STYLE_KEY               || chalk.blue.bold,
+  bracket:          global.FX_STYLE_BRACKET           || noop,
+  comma:            global.FX_STYLE_COMMA             || noop,
   colon:            global.FX_STYLE_COLON             || noop,
   list:             global.FX_STYLE_LIST              || list,
-  highlight:        global.FX_STYLE_HIGHLIGHT         || chalk.yellow.underline,
-  highlightCurrent: global.FX_STYLE_HIGHLIGHT_CURRENT || chalk.yellow.bold.underline,
+  highlight:        global.FX_STYLE_HIGHLIGHT         || chalk.black.bgYellow,
+  highlightCurrent: global.FX_STYLE_HIGHLIGHT_CURRENT || chalk.inverse,
   statusBar:        global.FX_STYLE_STATUS_BAR        || chalk.inverse,
 }
