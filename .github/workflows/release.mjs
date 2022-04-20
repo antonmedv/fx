0d556edd9e1a6b5bbbb782a931530d913537ed80
Anton Medvedev <anton@medv.io>
2022-04-20 23:16:58 +0200

    Update release script

--- .github/workflows/release.mjs
+++ .github/workflows/release.mjs
@@ -8,12 +8,16 @@ let goarch = [
   'arm64',
 ]
 
-await Promise.all(
-  goos.flatMap(GOOS =>
-    goarch.map(GOARCH =>
-      $`GOOS=${GOOS} GOARCH=${GOARCH} go build -o fx_${GOOS}_${GOARCH}`)))
+await $`go test ./...`
+
+let name = (GOOS, GOARCH) => `fx_${GOOS}_${GOARCH}` + (GOOS === 'windows' ? '.exe' : '')
 
 await Promise.all(
   goos.flatMap(GOOS =>
     goarch.map(GOARCH =>
-      $`gh release upload ${process.env.RELEASE_VERSION} fx_${GOOS}_${GOARCH}`)))
+      $`GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${name(GOOS, GOARCH)}`)))
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`gh release upload ${process.env.RELEASE_VERSION} ${name(GOOS, GOARCH)}`)))
