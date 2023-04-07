63fba11fc5795e571908c654777e6fa740701043
Anton Medvedev <anton@medv.io>
2023-04-07 15:33:19 +0200

    Update release process

---
+++ scripts/build.mjs
@@ -0,0 +1,26 @@
+const goos = [
+  'linux',
+  'darwin',
+  'windows',
+]
+const goarch = [
+  'amd64',
+  'arm64',
+]
+
+const name = (GOOS, GOARCH) => `fx_${GOOS}_${GOARCH}` + (GOOS === 'windows' ? '.exe' : '')
+
+const resp = await fetch('https://api.github.com/repos/antonmedv/fx/releases/latest')
+const {tag_name: latest} = await resp.json()
+
+await $`go mod download`
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${name(GOOS, GOARCH)}`)))
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`gh release upload ${latest} ${name(GOOS, GOARCH)}`)))
