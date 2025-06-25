5df014b92a2de7e57245750d200e3760525d1b14
Anton Medvedev <anton@medv.io>
2025-06-25 22:19:08 +0200

    Add wasd

--- help.go
+++ help.go
@@ -30,6 +30,7 @@ func usage(keyMap KeyMap) string {
     -s, --slurp           read all inputs into an array
     --yaml                parse input as YAML
     --strict              strict mode
+    --maze                start maze
 
   %v
 %v
--- internal/maze/main.go
+++ internal/maze/main.go
@@ -251,11 +251,11 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "left":
+		case "left", "a":
 			m.playerAngle -= 0.1
-		case "right":
+		case "right", "d":
 			m.playerAngle += 0.1
-		case "up":
+		case "up", "w":
 			speed := 0.2
 			dx := math.Cos(m.playerAngle) * speed
 			dy := math.Sin(m.playerAngle) * speed
@@ -265,7 +265,7 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if !m.isWallNoG(m.playerX, m.playerY+dy) {
 				m.playerY += dy
 			}
-		case "down":
+		case "down", "s":
 			speed := 0.2
 			dx := -math.Cos(m.playerAngle) * speed
 			dy := -math.Sin(m.playerAngle) * speed
