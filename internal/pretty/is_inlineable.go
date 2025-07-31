0feb0b98bbd63e21431db27998962946ae1fa747
Anton Medvedev <anton@medv.io>
2025-07-31 14:23:15 +0200

    Add inline objects

--- internal/pretty/is_inlineable.go
+++ internal/pretty/is_inlineable.go
@@ -5,26 +5,45 @@ import (
 )
 
 func isInlineable(n *jsonx.Node) bool {
-	if n.Kind == jsonx.Object && n.Key != "" {
-		return checkRecursive(n)
+	if n.Kind == jsonx.Object && len(n.Key) > 0 {
+		return isSimpleObject(n)
 	}
 	return false
 }
 
-func checkRecursive(n *jsonx.Node) bool {
-	if n.Kind == jsonx.Array {
-		return false
-	}
+func isSimpleObject(n *jsonx.Node) bool {
 	if n.Kind == jsonx.Object {
-		if n.Size <= 2 {
-			childrenInlineable := true
-			n.ForEach(func(child *jsonx.Node) {
-				childrenInlineable = childrenInlineable && checkRecursive(child)
-			})
-			return childrenInlineable
-		} else {
-			return false
+		isSimple := true
+		var firstKind jsonx.Kind
+		first := true
+		count := 0
+
+		n.ForEach(func(child *jsonx.Node) {
+			count++
+			if len(child.Key) > 10 {
+				isSimple = false
+				return
+			}
+			if first {
+				firstKind = child.Kind
+				first = false
+			} else if child.Kind != firstKind {
+				isSimple = false
+				return
+			}
+
+			if child.Kind != jsonx.Number &&
+				child.Kind != jsonx.Bool &&
+				(child.Kind != jsonx.String || len(child.Value) > 20) {
+				isSimple = false
+			}
+		})
+
+		if (firstKind == jsonx.String && count > 2) ||
+			((firstKind == jsonx.Number || firstKind == jsonx.Bool) && count > 3) {
+			isSimple = false
 		}
+		return isSimple
 	}
-	return true
+	return false
 }
