0d1d7d7555f1fc858dc7e06ddff9e2d3debb451a
Anton Medvedev <anton@medv.io>
2025-08-29 15:26:02 +0200

    Update complete.zsh

--- internal/complete/complete.zsh
+++ internal/complete/complete.zsh
@@ -1,14 +1,20 @@
-_fx_complete() {
-  local reply
-  reply=("${(@f)$(COMP_ZSH="${LBUFFER}" fx)}")
-  if (( ${#reply} )); then
-    local -a insert display
-    for line in "${reply[@]}"; do
-      display+=("${line%%$'\t'*}")
-      insert+=("${line#*$'\t'}")
-    done
-    compadd -f -S '' -d display -a insert
-  fi
+#compdef fx
+
+_fx() {
+    local reply
+    reply=("${(@f)$(COMP_ZSH="${LBUFFER}" fx)}")
+    if (( ${#reply} )); then
+        local -a insert display
+        for line in "${reply[@]}"; do
+            display+=("${line%%$'\t'*}")
+            insert+=("${line#*$'\t'}")
+        done
+        compadd -f -S '' -d display -a insert
+    fi
 }
 
-compdef _fx_complete fx
+if [ "$funcstack[1]" = "_fx" ]; then
+    _fx "$@"
+else
+    compdef _fx fx
+fi
