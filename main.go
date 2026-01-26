ebe60e709710b14742ef1ebb552c8a8d368c5045
Anton Medvedev <anton@medv.io>
2026-01-26 20:45:04 +0100

    Add navigation key bindings for preview pane (top, bottom, paging, and half-paging)

--- main.go
+++ main.go
@@ -638,9 +638,34 @@ func (m *model) handlePreviewKey(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case key.Matches(msg, keyMap.Quit),
 			key.Matches(msg, keyMap.Preview):
 			m.showPreview = false
+			return m, nil
 
 		case key.Matches(msg, keyMap.Print):
 			return m, m.print()
+
+		case key.Matches(msg, keyMap.GotoTop):
+			m.preview.GotoTop()
+			return m, nil
+
+		case key.Matches(msg, keyMap.GotoBottom):
+			m.preview.GotoBottom()
+			return m, nil
+
+		case key.Matches(msg, keyMap.HalfPageUp):
+			m.preview.HalfPageUp()
+			return m, nil
+
+		case key.Matches(msg, keyMap.HalfPageDown):
+			m.preview.HalfPageDown()
+			return m, nil
+
+		case key.Matches(msg, keyMap.PageUp):
+			m.preview.PageUp()
+			return m, nil
+
+		case key.Matches(msg, keyMap.PageDown):
+			m.preview.PageDown()
+			return m, nil
 		}
 	}
 	m.preview, cmd = m.preview.Update(msg)
